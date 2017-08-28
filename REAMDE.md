e8b2f03931e9240da561b1c99e7588baa2b36553
William Luangphinith <william.luangphinith@epitech.eu>
2017-08-28 15:45:55 +0200

    Update readme, change a bit the logic in index

--- REAMDE.md
+++ REAMDE.md
@@ -1,7 +1,7 @@
 Dial-Once
 
-Set-up environment:
-- Copy the content from .env.tpl into a new file .env, or use your own hashing key
+This modules is used for encryption/decryption of datas.
+It uses GCM (Galois/Counter Mode).
 
 Examples:
 
@@ -11,16 +11,18 @@ const security = require('node-security');
 
 const data = 'Hello World !';
 
-// It will use the hashing key from the .env file to encrypt data
-security.encrypt(data);
+const securised = security.encrypt(data, key);
+
+securised: { content: '1016acd5af5b7b91985fc36d7b',
+  vector: '73d1b9d375fa33c355b185dd',
+  tag: 'bdea27df226f54da5bb9ed12e4312e12' }
 ```
 
 - Decrypt data
 ```
 const security = require('node-security');
 
-const data = 'Hello World !';
+security.decrypt(securised, key);
 
-// It will use the hashing key from the .env file to decrypt data
-security.decrypt(data);
+output: 'Hello World !';
 ```
--- src/index.js
+++ src/index.js
@@ -1,68 +1,12 @@
 const crypto = require('crypto');
 const assert = require('assert');
 
+function string2Boolean(str) {
+  return (typeof str === 'boolean' ? `${str}` : str);
+}
 
-/**
- * Enum for two state values.
- * @readonly
- * @enum {number}
- */
-const actionEnum = {
-  ENCRYPT: 0,
-  DECRYPT: 1
-};
-
-/**
-   * Encrypt/Decrypt incoming data depending on action
-   * @param  {Array|string|boolean|object} data - data to encrypt/decrypt.
-   * In case of encryption, data can be Array of string or string.
-   * In case of decryption, data can be an Array of objects or an object
-   * @param {enum} action - action to perform. Can be either ENCRYPT or DECRYPT
-   * @return {Array|string|boolean|object} - encrypted Array of objects or one object / decrypted Array of strings or one string
-*/
-function performAction(data, key, action) {
-  if ([null, undefined].includes(data) || [null, undefined].includes(key)) {
-    return data;
-  }
-
-  const isArray = Array.isArray(data);
-  const queue = isArray ? [...data] : [data];
-  const result = [];
-
-  for (let item of queue) {
-    if (action === actionEnum.ENCRYPT) {
-      // if already encrypted
-      if (item.content && item.vector && item.tag) {
-        result.push(item);
-        continue; // eslint-disable-line no-continue
-      }
-
-      const itemValue = typeof item === 'boolean' ? `${item}` : item; // converting boolean to string
-      const vector = crypto.randomBytes(12);
-      const cipher = crypto.createCipheriv('aes-256-gcm', key, vector);
-      const encryptedItem = `${cipher.update(itemValue, 'utf8', 'hex')}${cipher.final('hex')}`;
-      const tag = cipher.getAuthTag();
-      result.push({
-        content: encryptedItem,
-        vector: vector.toString('hex'),
-        tag: tag.toString('hex')
-      });
-    } else if (action === actionEnum.DECRYPT) {
-      // if was not encrypted
-      if (!item.content || !item.vector || !item.tag) {
-        result.push(item);
-        continue; // eslint-disable-line no-continue
-      }
-
-      const decipher = crypto.createDecipheriv('aes-256-gcm', key, Buffer.from(item.vector, 'hex'));
-      decipher.setAuthTag(Buffer.from(item.tag, 'hex'));
-      const decryptedValue = `${decipher.update(item.content, 'hex', 'utf8')}${decipher.final('utf8')}`;
-      // converting string to boolean
-      const normalizedValue = ['true', 'false'].includes(decryptedValue) ? decryptedValue === 'true' : decryptedValue;
-      result.push(normalizedValue);
-    }
-  }
-  return isArray ? result : result[0];
+function boolean2String(bool) {
+  return (['true', 'false'].includes(bool) ? bool === 'true' : bool);
 }
 
 module.exports = {
@@ -72,9 +16,38 @@ module.exports = {
    * @param  {string} key        - key used for encryption
    * @return {Array|object}      - Array of objects with encrypted data or one object
    */
-  encrypt: (data, key) => performAction(data, key, actionEnum.ENCRYPT),
+  encrypt: (data, key) => {
+    // if already encrypted
+    if ([null, undefined].includes(data) || [null, undefined].includes(key)) {
+      return data;
+    }
+    
+    const isArray = Array.isArray(data);
+    const queue = isArray ? [...data] : [data];
+    const result = [];
+    
+    for (let item of queue) {
+      if (item.content && item.vector && item.tag) {
+        result.push(item);
+        continue; // eslint-disable-line no-continue
+      }
+
+      const itemValue = string2Boolean(item);
+      const vector = crypto.randomBytes(12);
+      const cipher = crypto.createCipheriv('aes-256-gcm', key, vector);
+      const encryptedItem = `${cipher.update(itemValue, 'utf8', 'hex')}${cipher.final('hex')}`;
+      const tag = cipher.getAuthTag();
+      result.push({
+        content: encryptedItem,
+        vector: vector.toString('hex'),
+        tag: tag.toString('hex')
+      });
+    }
+    
+    return isArray ? result : result[0];
+  },
   /**
-   * Decrypt Array of objects or one object of hte following structure:
+   * Decrypt Array of objects or one object of the following structure:
    * {
    *   content: {string}, - encrypted content
    *   vector: {string},  - encryption vector
@@ -84,5 +57,29 @@ module.exports = {
    * @param  {string} key        - key used for decryption
    * @return {Array|string}      - Array of decrypted strings or decrypted string
    */
-  decrypt: (hash, key) => performAction(hash, key,actionEnum.DECRYPT)
+  decrypt: (data, key) => {
+    if ([null, undefined].includes(data) || [null, undefined].includes(key)) {
+      return data;
+    }
+    
+    const isArray = Array.isArray(data);
+    const queue = isArray ? [...data] : [data];
+    const result = [];
+    
+    for (let item of queue) {
+      // if was not encrypted
+      if (!item.content || !item.vector || !item.tag) {
+        result.push(item);
+        continue; // eslint-disable-line no-continue
+      }
+
+      const decipher = crypto.createDecipheriv('aes-256-gcm', key, Buffer.from(item.vector, 'hex'));
+      decipher.setAuthTag(Buffer.from(item.tag, 'hex'));
+      const decryptedValue = `${decipher.update(item.content, 'hex', 'utf8')}${decipher.final('utf8')}`;
+      const normalizedValue = boolean2String(decryptedValue);
+      result.push(normalizedValue);
+    }
+    
+    return isArray ? result : result[0];
+  }
 };
